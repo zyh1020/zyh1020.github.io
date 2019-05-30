f14fec70affc8f5684f29248d07fd03fd09200cb
zyh1020 <zyh1020@qq.com>
2019-05-30 15:06:21 +0800

    Site updated: 2019-05-30 15:06:19

--- dist/music.js
+++ dist/music.js
@@ -1,25 +1,33 @@
 const ap = new APlayer({
-    container: document.getElementById('player'),
+    container: document.getElementById('aplayer'),
     fixed: true,
     autoplay: false,
+	loop:'all',
     audio: [
-      {
-        name: "PDD洪荒之力",
-        artist: '徐梦圆',
-        url: 'http://up.mcyt.net/?down/39868.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/84.jpg',
+	 {
+        name: '远走高飞',
+        artist: '金志文',
+        url: 'http://www.ytmp3.cn/down/47236.mp3',
+        cover: 'http://img.ytmp3.cn/image/32.jpg',
       },
       {
-        name: '9420',
-        artist: '麦小兜',
-        url: 'http://up.mcyt.net/?down/45967.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/8.jpg',
+        name: "生僻字",
+        artist: '陈柯宇',
+        url: 'http://www.ytmp3.cn/down/56174.mp3',
+        cover: 'http://img.ytmp3.cn/image/86.jpg',
       },
       {
-        name: '风筝误',
-        artist: '刘珂矣',
-        url: 'http://up.mcyt.net/?down/46644.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/96.jpg',
+        name: '日不落',
+        artist: '蔡依林',
+        url: 'http://www.ytmp3.cn/down/39102.mp3',
+        cover: 'http://img.ytmp3.cn/image/27.jpg',
+      },
+	  {
+        name: '成都',
+        artist: '赵雷',
+        url: 'http://www.ytmp3.cn/down/58479.mp3',
+        cover: 'http://img.ytmp3.cn/image/47.jpg',
       }
+      
     ]
 });
