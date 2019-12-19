0124173303ce68818a0728f62420b3ef62b047d4
zyh1020 <898362059@qq.pcom>
2019-12-19 11:41:55 +0800

    Site updated: 2019-12-19 11:41:53

--- dist/music.js
+++ dist/music.js
@@ -7,26 +7,26 @@ const ap = new APlayer({
 	 {
         name: '远走高飞',
         artist: '金志文',
-        url: 'http://www.ytmp3.cn/down/47236.mp3',
-        cover: 'http://img.ytmp3.cn/image/32.jpg',
+        url: 'http://qiniuyun.zouyuhang.club/music%E9%87%91%E5%BF%97%E6%96%87%20-%20%E8%BF%9C%E8%B5%B0%E9%AB%98%E9%A3%9E.flac',
+        cover: 'http://qiniuyun.zouyuhang.club/%E8%BF%9C%E8%B5%B0%E9%AB%98%E9%A3%9E.jpg',
       },
       {
         name: "生僻字",
         artist: '陈柯宇',
-        url: 'http://www.ytmp3.cn/down/56174.mp3',
-        cover: 'http://img.ytmp3.cn/image/86.jpg',
+        url: 'http://qiniuyun.zouyuhang.club/music%E8%94%A1%E4%BE%9D%E6%9E%97%20-%20%E6%97%A5%E4%B8%8D%E8%90%BD.mp3',
+        cover: 'http://qiniuyun.zouyuhang.club/%E7%94%9F%E5%83%BB%E5%AD%97.jpg',
       },
       {
         name: '日不落',
         artist: '蔡依林',
-        url: 'http://www.ytmp3.cn/down/39102.mp3',
-        cover: 'http://img.ytmp3.cn/image/27.jpg',
+        url: 'http://qiniuyun.zouyuhang.club/music%E8%94%A1%E4%BE%9D%E6%9E%97%20-%20%E6%97%A5%E4%B8%8D%E8%90%BD.mp3',
+        cover: http://qiniuyun.zouyuhang.club/%E6%97%A5%E4%B8%8D%E8%90%BD.jpg',
       },
 	  {
         name: '成都',
         artist: '赵雷',
-        url: 'http://www.ytmp3.cn/down/58479.mp3',
-        cover: 'http://img.ytmp3.cn/image/47.jpg',
+        url: 'http://qiniuyun.zouyuhang.club/music%E8%B5%B5%E9%9B%B7%20-%20%E6%88%90%E9%83%BD.mp3',
+        cover: 'http://qiniuyun.zouyuhang.club/%E6%88%90%E9%83%BD.jpg',
       }
       
     ]
