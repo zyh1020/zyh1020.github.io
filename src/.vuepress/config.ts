d73e63f904fc78c5399a630184aaed9330f661b9
zyh1020 <898362059@qq.pcom>
2025-08-30 16:47:55 +0800

    更改访问路径

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/vuepress/",
   lang: "zh-CN",
   title: "Mr.Zyh",
   description: "Zyh的博客",
